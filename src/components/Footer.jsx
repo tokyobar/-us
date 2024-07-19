6788d08121d35de3b95a3451258d97161ffd81ea
tokyobar <tokyobar.us@gmail.com>
2024-07-19 14:08:34 +0000

    test: colour update

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -46,7 +46,7 @@ const Footer = () => (
 
         <div className="w-full flex justify-between items-center md:flex-row flex-col pt-6 border-t-[1px] border-t-[#3F3E45]">
             <p className="font-poppins font-normal text-center text-[18px] leading-[27px] text-white">
-                Copyright Ⓒ 2022 HooBank. All Rights Reserved.
+            <a href="https://wa.me/971563901109" target="_blank" title="971563901109">Contact Website Developer</a>
             </p>
 
             <div className="flex flex-row gap-[24px] md:mt-0 mt-6">
