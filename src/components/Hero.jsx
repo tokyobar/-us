589208a3a131fd4875061096d82554a15940c587
tokyobar <tokyobar.us@gmail.com>
2024-07-19 12:37:27 +0000

    test: hero update

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -24,28 +24,25 @@ const Hero = () => {
                         className="w-[32px] h-[32px]"
                     />
                     <p className={`${styles.paragraph} ml-2`}>
-                        <span className="text-white">20%</span> Discount For{' '}
-                        <span className="text-white">1 Month</span> Account
+                        <span className="text-white">100%</span> Great{' '}
+                        <span className="text-white">Flovours</span> Guarenteed
                     </p>
                 </div>
 
                 <div className="flex flex-row justify-between items-center w-full">
                     <h1 className="flex-1 font-poppins font-semibold ss:text-[72px] text-[52px] text-white ss:leading-[100.8px] leading-[75px]">
-                        The Next <br className="sm:block hidden" />{' '}
-                        <span className="text-gradient">Generation</span>{' '}
+                        All New <br className="sm:block hidden" />{' '}
+                        <span className="text-gradient">TOKYOBAR</span>{' '}
                     </h1>
                     <div className="ss:flex hidden md:mr-4 mr-0">
                         <GetStarted />
                     </div>
                 </div>
 
-                <h1 className="font-poppins font-semibold ss:text-[68px] text-[52px] text-white ss:leading-[100.8px] leading-[75px] w-full">
-                    Payment Method.
-                </h1>
+    
                 <p className={`${styles.paragraph} max-w-[470px] mt-5`}>
-                    Our team of experts uses a methodology to identify the
-                    credit cards most likely to fit your needs. We examine
-                    annual percentage rates, annual fees.
+                Experience the vibrant flavors and sleek design,
+                capturing the spirit of the city in every puff. 
                 </p>
             </motion.div>
 
